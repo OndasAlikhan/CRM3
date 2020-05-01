3f7ef8d3040226258d40059f0b5ff787fc530e96
nagibator9986 <alihan.ondas@gmail.com>
2020-05-01 19:00:15 +0600

    Commit message luboiy

--- CRM3/Models/Customer.cs
+++ CRM3/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace CRM3.Models
     public class Customer
     {
         public int ID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string FullName { get; set; }
         public string Phone { get; set; }
 
